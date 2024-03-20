fbeeb28bff34a9938840cdda9ae4df4380525f36
salopez <slopez@bdtglobal.com>
2024-03-20 12:29:21 -0300

    Proyecto Inicial

---
+++ App/Support/Dependencies.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Support;
+
+public static class Dependencies
+{
+    public static IServiceCollection AddApp(this IServiceCollection services)
+    {
+        RegisterServices(services);
+        return services;
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+
+    }
+}
---
+++ Infrastructure/Support/Dependencies.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Support;
+
+public static class Dependencies
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        RegisterRepositories(services);
+        return services;
+    }
+
+    private static void RegisterRepositories(IServiceCollection services)
+    {
+
+    }
+}
